8a12bb09badf7a334489c805fb70089af37dbbd7
wookyoungkim <viliketh1s98@naver.com>
2021-04-18 23:31:29 +0900

    uuid로 room마다 고유 id 부여

--- server.js
+++ server.js
@@ -2,5 +2,19 @@ const express = require('express')
 const app = express()
 const server = require('http').Server(app)
 const io = require('socket.io')(server)
+const { v4: uuidV4 } = require('uuid')
+
+app.set('view engine', 'ejs') // view engine 설정
+app.use(express.static('public')) // html, css등 static file들 경로 설정
+
+// creating a new room -> redirect to the room
+app.get('/', (req, res) =>{
+    res.redirect(`/${uuidV4()}`) // dynamic url
+})
+
+// 해당 room으로 들어가게
+app.get('/:room', (req, res) =>{
+    res.render('room', { roomId: req.params,room })
+})
 
 server.listen(3000)
